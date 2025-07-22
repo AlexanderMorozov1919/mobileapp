fa582bae7c5b538b197b1ac355b5c8b899403a10
Max <53438629+PhosFactum@users.noreply.github.com>
2025-07-22 13:13:42 +0300

    Konkov: docker added and readme changed (#111)
    * allegy handler deleted
    
    * hospital fixed
    
    * Squashed commit of the following:
    
    commit d00f41615df4aa0d8fbd560f296588e7563b02b9
    Author: Konkov Maxim <phosphorus.work@gmail.com>
    Date:   Wed Jul 16 14:58:58 2025 +0300
    
        feat(CORS): cors is ready
    
    commit 9306d8a524a90ca6e30b9fd392041676bca76985
    Author: Konkov Maxim <phosphorus.work@gmail.com>
    Date:   Wed Jul 16 13:30:29 2025 +0300
    
        feat(docs): swagger is ready; fix(gorm): standart fields written manually
    
    commit 07e4e803f283bd00bf385d1a491c6a5c2ac5873b
    Author: Konkov Maxim <phosphorus.work@gmail.com>
    Date:   Wed Jul 16 11:38:12 2025 +0300
    
        deleted .env
    
    * fix(docs): swagger annotation fixed; fix(models): doctors email deleted
    
    * services connected to handlers
    
    * docs(swagger): annotations looks better
    
    * upd errors
    
    * SMPS
    
    * fix: dev now working, fixed conflicts
    
    * SMPS_BY_CALLID
    
    * all notes see in my Telegram message
    
    * fix init
    
    * SMPS_FIX_ANR_ROUTES
    
    * patient fixed
    
    * Specialization_Table_Created
    
    * SMPS_BY_CALLID
    
    * SMPS_FIX_ANR_ROUTES
    
    * Specialization_Table_Created
    
    * FINALLY
    
    * add updateReceptionSMP
    
    * fix conflicts
    
    * wqeewq
    
    * add id for auth
    
    * add id for auth2
    
    * patients filters fixed
    
    * JSONB_FOR_RECEPS
    
    * SMPS_JSONB
    
    * fix(swagger): generating docs correctly with new JSONB (good solve)
    
    * docs: readme changed; feat: created swaggen+run batch script
    
    * docs(codes): show right statuses of responses; fix: little warnings
    
    * filter and order full added to hospital
    
    * docs: readme changed
    
    * docs(README): added icons
    
    * feat(docker): added docker support
    
    ---------
    
    Co-authored-by: ALYoNka <khrllw@gmail.com>
    Co-authored-by: Kharlova Alyona <161920032+Khrllw@users.noreply.github.com>
    Co-authored-by: maximdudko17 <maxim.dudko17@gmail.com>
    Co-authored-by: maximdudko17 <90906817+maximdudko17@users.noreply.github.com>
    Co-authored-by: LendorD <Wolf1980M@gmail.com>
    Co-authored-by: Dmitry Lutsenko <90717837+LendorD@users.noreply.github.com>

---
+++ internal/domain/models/recptions_smp.go
@@ -0,0 +1,25 @@
+package models
+
+type ReceptionSMPShortResponse struct {
+	Id          uint   `json:"id"`
+	PatientName string `json:"patient_name" example:"Иванов Иван"` // ФИО пациента
+	Diagnosis   string `json:"diagnosis" example:"ОРВИ"`
+}
+
+// type ReceptionSMPResponse struct {
+// 	Id              uint   `json:"id"`
+// 	PatientName     string `json:"patient_name" example:"Иванов Иван"` // ФИО пациента
+// 	Diagnosis       string `json:"diagnosis" example:"ОРВИ" rus:"Диагноз"`
+// 	Recommendations string `json:"recommendations" example:"Постельный режим" rus:"Рекомендации"`
+// 	MedServices []MedServicesResponse  `json:"med_services" rus:"Медицинские услуги"`
+// }
+
+type ReceptionSMPResponse struct {
+	ID                 uint                  `json:"id"`
+	PatientName        string                `json:"patient_name" example:"Иванов Иван"`
+	Diagnosis          string                `json:"diagnosis" example:"ОРВИ"`
+	Recommendations    string                `json:"recommendations" example:"Постельный режим"`
+	Specialization     string                `json:"specialization" example:"Терапевт"`
+	SpecializationData interface{}           `json:"specialization_data"`
+	MedServices        []MedServicesResponse `json:"med_services"`
+}
